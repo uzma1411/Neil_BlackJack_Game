b4b6d78cb8c0f45f17a917d60da40631ca96ab18
uzma1411 <47413610+uzma1411@users.noreply.github.com>
2023-06-19 07:35:24 +0530

    Updated sketch.js - class 47

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
 var playerTotal = 0
 var dealerTotal = 0
 var cardsimage = [];
-var cards = [];
+var cards = {};
+var playerHand = ["./cards/6-D.png", "./cards/9-D.png"]
+var dealerHand = ["./cards/6-D.png", "./cards/9-D.png"]
+var gameState = 0
+var dealBtn, hitBtn, standBtn
+var dealt = false
+
+
 
 let types = ["C", "D", "H", "S"];
 let values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "A", "J", "K", "Q"];
@@ -20,46 +27,112 @@ function setup() {
 
   createCanvas(windowWidth, windowHeight);
 
-  for (let i = 0; i < types.length; i++) {
-    for (let j = 0; j < values.length; j++) {
-      cards.push(values[j] + "-" + types[i]);
+  for (let i = 0; i < cardsimage.length; i++) {
+    if (parseInt(cardsimage[i][9]) === 0 || (cardsimage[i][8]) == "J" || (cardsimage[i][8]) == "K" || (cardsimage[i][8]) == "Q") {
+      cards[cardsimage[i]] = 10
+    }
+    else if ((cardsimage[i][8]) == "A") {
+      cards[cardsimage[i]] = 11
+    }
+    else {
+      cards[cardsimage[i]] = parseInt(cardsimage[i][8])
     }
   }
   console.log(cards)
 
+  dealBtn = createButton("Deal");
+  dealBtn.position(width / 2, 600);
+  dealBtn.size(100, 50);
+  dealBtn.mousePressed(()=>{
+    dealt=true; 
+    console.log(dealt)
+  })
+  dealBtn.style("display","none");
+
+
 }
 
 function draw() {
-  background(0);
-  
-  c = createSprite(200, 200, 50, 300);
-  c.addImage(loadImage(cardsimage[0]));
-  c.scale = 0.5;
-  
-  
-  drawSprites();
+  background(0);  
+
+  if (gameState === 0) {
+    textSize(60)
+    text("Blackjack", width / 2 - 100, height / 2)
+    textSize(40)
+    text("Press Enter to start", width / 2 - 140, height / 2 + height / 10)
+    if (keyDown("enter")) {
+      gameState = 1
+    }
+  }
+
+  if (gameState === 1) {
+    dealerTotal = getTotal(dealerHand);
+    // console.log(dealerHand);
+    playerTotal = getTotal(playerHand)
+    drawDealerHand(dealerHand);
+    drawPlayerHand(playerHand);
+    if (!(dealt)){
+      dealBtn.style("display","block")
+    }
+    drawSprites();
+    textSize(50)
+    fill("White")
+    text(dealerTotal, width * 3 / 4, height / 4)
+    textSize(50)
+    fill("White")
+    text(playerTotal, width * 3 / 4, height * 3 / 4)
+
+
+
+
+
+  }
+
+
+
 }
 
 
-function getTotal(imageName) {
-  try {
-    if (imageName.charAt(1) == 0) {
-      total = 10
+function getTotal(hand) {
+  total = 0
+  aces = 0
+  for (var i = 0; i < hand.length; i++) {
+    total += cards[hand[i]];
+    console.log([hand[i]]);
+    if (cards[hand[i]] == 11) {
+      aces += 1
     }
-    else {
-      total = parseInt(imageName.charAt(0))
-    }
-  }
-  catch (NaN) {
-    if (imageName.charAt(0) == "a") {
-      total = 11
-    }
-    else {
-      total = 10
+    while (aces > 0 && total > 21) {
+      total -= 10
+      aces -= 1
     }
 
   }
+  return total
 
+}
+
+function drawDealerHand(dealerHand) {
+  offset = 200
+  for (var i = 0; i < dealerHand.length; i++) {
+    card = createSprite(400 - offset, 200, 50, 300)
+    card.addImage(loadImage(dealerHand[i]));
+    card.scale = 0.35;
+    offset -= 35
+
+  }
+
+}
+
+function drawPlayerHand(playerHand) {
+  offset = 200
+  for (var i = 0; i < playerHand.length; i++) {
+    card = createSprite(400 - offset, 500, 50, 300)
+    card.addImage(loadImage(playerHand[i]));
+    card.scale = 0.35;
+    offset -= 35
+
+  }
 
 }
 
